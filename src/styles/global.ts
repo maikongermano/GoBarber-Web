5eecda2ddc93b05709c2dc9acaaf76503cce2ccf
Maikon Germano <mk_germano@hotmail.com>
2020-07-01 01:17:50 -0300

    'master'

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,10 +11,20 @@ export default createGlobalStyle`
     body {
       background: #312e38;
       color: #FFF;
-      -webkit-font-smoothing: antialiased
+      -webkit-font-smoothing: antialiased;
     }
 
     body, input, button {
+      font-family: 'Roboto Slab', serif;
+      font-size: 16px;
 
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+      font-weight: 500;
+    }
+
+    button {
+      cursor: pointer;
     }
 `;
